114d75868abe6ec11be4bd28c5b5843ceb3a1a3d
Muhammed fahis <fahis143.sa@gamil.com>
2025-03-19 14:27:40 +0530

    Added solution for Best Time to Buy and Sell Stock

---
+++ challenge-2/Best_time_to_buyandsell_stock.py
@@ -0,0 +1,29 @@
+"""
+Best Time to Buy and Sell Stock 
+
+You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+"""
+
+
+class Solution:
+    def maxProfit(self, prices):
+        #Two-pointer
+        left,right=0,1
+        profit=0
+
+        while right < len(prices):
+            #profitable
+            if prices[right] > prices[left]:
+                profit=max(profit,prices[right] - prices[left])
+            else:
+            #nonprofitable
+                left=right
+            right += 1
+        return profit
+
+stock= Solution()
+print(stock.maxProfit([7,1,5,3,6,4]))
